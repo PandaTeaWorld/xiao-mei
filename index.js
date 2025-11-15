2f5fcdc53256cf66531dca913dac1f3ad94947e1
PandaTea <69850870+PandaTeaWorld@users.noreply.github.com>
2025-11-15 23:35:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits, ActivityType, } = require('discord.js');
+const { token } = require('./config.json');
+const { player } = require("discord-player"); 
+const { Player } = require('discord-player');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates, ] });
+
+
+// commands
+client.commands = new Collection();
+const commandFoldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(commandFoldersPath);
+
+for (const commandFolder of commandFolders) {
+    const commandsPath = path.join(commandFoldersPath, commandFolder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+
+// events
+const eventFoldersPath = path.join(__dirname, 'events');
+const eventFolders = fs.readdirSync(eventFoldersPath);
+
+for (const eventFolder of eventFolders) {
+    const eventsPath = path.join(eventFoldersPath, eventFolder);
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
+        }
+    }
+}
+
+client.login(token);
