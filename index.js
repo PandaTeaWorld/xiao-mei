cb9f400a0d02e88041abea2cdc64d31f853b195c
Andy <andyquacht@gmail.com>
2025-11-20 22:20:15 -0800

    Add path module import in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { token } = require('./config.json');
 const { player } = require("discord-player"); 
 const { Player } = require('discord-player');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates, ] });
+const path = require('node:path');
 
 
 // commands
